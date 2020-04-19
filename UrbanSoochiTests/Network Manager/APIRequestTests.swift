2a4bcb861d86adfc0586c672bf5ba20fecd46042
Vinayak Hejib <vinayak.gh@thoughtworks.com>
2020-04-19 17:27:23 +0530

    Commit ID #102
    Description:  Added tests for Network manager classes

---
+++ UrbanSoochiTests/Network Manager/APIRequestTests.swift
@@ -0,0 +1,58 @@
+//
+//  APIRequestTests.swift
+//  UrbanSoochiTests
+//
+//  Created by Vinayak.gh on 17/04/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import XCTest
+@testable import UrbanSoochi
+
+class APIRequestTests: XCTestCase {
+
+    func testForBaseUrl() {
+        XCTAssertEqual("baseUrl", self.baseUrl())
+    }
+
+    func testForPath() {
+        XCTAssertEqual("path", self.path())
+    }
+
+    func testForMethod() {
+        XCTAssertEqual("GET", self.method())
+    }
+
+    func testForData() {
+        XCTAssertNil(self.data())
+    }
+
+    func testForError() {
+        XCTAssertNil(self.error())
+    }
+}
+
+extension APIRequestTests: APIRequest {
+
+    typealias ModelType = CitiesAPIData
+
+    func baseUrl() -> String {
+        return "baseUrl"
+    }
+
+    func path() -> String {
+        return "path"
+    }
+
+    func method() -> String {
+        return "GET"
+    }
+
+    func data() -> Data? {
+        return nil
+    }
+
+    func error() -> NetworkError? {
+        return nil
+    }
+}
