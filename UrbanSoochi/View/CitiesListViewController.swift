39c96dd94c0ee2a31c9ba0ee02896baf96f9ec4c
Vinayak G Hejib <50238254+bhaminCoder@users.noreply.github.com>
2024-07-08 20:33:26 +0530

    Update CitiesListViewController.swift

--- UrbanSoochi/View/CitiesListViewController.swift
+++ UrbanSoochi/View/CitiesListViewController.swift
@@ -178,4 +178,4 @@ extension CitiesListViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate
 extension CitiesListViewController: UITableViewDelegate {}
 
-//TODO: Remove this line after PR Review
+//
